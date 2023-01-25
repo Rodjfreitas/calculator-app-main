7d5c38a9f9837e3766608a596f35d10a135308fb
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-25 14:18:33 -0300

    style: reorganization of css variables through classes.

--- js/javasript.js
+++ js/javasript.js
@@ -1,76 +1,30 @@
 const theme1 = document.querySelector('#chk1')
 const theme2 = document.querySelector('#chk2')
 const theme3 = document.querySelector('#chk3')
+const html = document.querySelector('html');
 
 function theme1click(){
+  html.classList.remove('theme2');
+  html.classList.remove('theme3');
   const ball = document.querySelector('.ball');
-    ball.style.transform = "translateX(0px)";
-  const calculator = document.querySelector('#calculator');
-    calculator.classList.remove('active');
-  const Keypad = document.querySelector('#keypad');
-    Keypad.classList.remove('active');
-  const result = document.querySelector('.result');
-    result.classList.remove('active');
-  const body = document.querySelector('body');
-    body.classList.remove('active');
-  const footer = document.querySelector('.attribution');
-    footer.classList.remove('active');  
-    calculator.classList.remove('active2');  
-    Keypad.classList.remove('active2');  
-    result.classList.remove('active2');  
-    body.classList.remove('active2');  
-    footer.classList.remove('active2');
-  }
-
-
-
-
-
-
-function theme2click(){
-const ball = document.querySelector('.ball');
-  ball.style.transform = "translateX(26px)";
-const calculator = document.querySelector('#calculator');
-  calculator.classList.toggle('active');
-const Keypad = document.querySelector('#keypad');
-  Keypad.classList.toggle('active');
-const result = document.querySelector('.result');
-  result.classList.toggle('active');
-const body = document.querySelector('body');
-  body.classList.toggle('active');
-const footer = document.querySelector('.attribution');
-  footer.classList.toggle('active');
-  calculator.classList.remove('active2');
-  Keypad.classList.remove('active2');
-  result.classList.remove('active2');
-  body.classList.remove('active2');
-  footer.classList.remove('active2');
+  ball.style.transform = "translateX(0px)";
 }
 
 
-
-function theme3click(){
+function theme2click(){  
+  html.classList.remove('theme3');
+  html.classList.add('theme2');
   const ball = document.querySelector('.ball');
-    ball.style.transform = "translateX(50px)";
-  const calculator = document.querySelector('#calculator');
-    calculator.classList.toggle('active2');
-  const Keypad = document.querySelector('#keypad');
-    Keypad.classList.toggle('active2');
-  const result = document.querySelector('.result');
-    result.classList.toggle('active2');
-  const body = document.querySelector('body');
-    body.classList.toggle('active2');
-  const footer = document.querySelector('.attribution');
-    footer.classList.toggle('active2');
-    calculator.classList.remove('active');
-    Keypad.classList.remove('active');
-    result.classList.remove('active');
-    body.classList.remove('active');
-    footer.classList.remove('active');
-  }
+  ball.style.transform = "translateX(26px)";
+}
 
+function theme3click(){  
+  html.classList.remove('theme2');
+  html.classList.add('theme3');
+  const ball = document.querySelector('.ball');
+  ball.style.transform = "translateX(50px)";
+}
 
 theme1.addEventListener('click',theme1click);
 theme2.addEventListener('click',theme2click);
 theme3.addEventListener('click',theme3click);
-
